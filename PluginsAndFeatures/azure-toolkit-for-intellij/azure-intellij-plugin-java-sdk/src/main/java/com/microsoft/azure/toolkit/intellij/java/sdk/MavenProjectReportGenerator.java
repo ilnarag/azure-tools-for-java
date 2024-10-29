4b7190a6ca5eacef14c1d33b09b02ee4b1d1faff
Srikanta <srnagar@microsoft.com>
2024-10-29 00:21:53 -0700

    use telemetry client

--- PluginsAndFeatures/azure-toolkit-for-intellij/azure-intellij-plugin-java-sdk/src/main/java/com/microsoft/azure/toolkit/intellij/java/sdk/MavenProjectReportGenerator.java
+++ PluginsAndFeatures/azure-toolkit-for-intellij/azure-intellij-plugin-java-sdk/src/main/java/com/microsoft/azure/toolkit/intellij/java/sdk/MavenProjectReportGenerator.java
@@ -18,16 +18,18 @@ import com.intellij.psi.search.GlobalSearchScope;
 import com.intellij.psi.util.PsiTreeUtil;
 import com.intellij.psi.xml.XmlFile;
 import com.intellij.psi.xml.XmlTag;
-import com.microsoft.azure.toolkit.intellij.java.sdk.utils.MavenUtils;
+import com.microsoft.applicationinsights.TelemetryClient;
+import com.microsoft.applicationinsights.TelemetryConfiguration;
+import com.microsoft.applicationinsights.telemetry.EventTelemetry;
 import com.microsoft.azure.toolkit.intellij.java.sdk.report.*;
-import com.microsoft.azure.toolkit.intellij.java.sdk.telemetry.*;
+import com.microsoft.azure.toolkit.intellij.java.sdk.utils.MavenUtils;
 import kotlin.Unit;
 import kotlin.coroutines.Continuation;
+import org.apache.commons.codec.digest.DigestUtils;
 import org.jetbrains.idea.maven.project.MavenProjectsManager;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
-import java.time.OffsetDateTime;
 import java.util.*;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
@@ -37,52 +39,51 @@ import java.util.stream.Collectors;
 
 public final class MavenProjectReportGenerator implements ProjectActivity, DumbAware, ProjectManagerListener {
     private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper().setSerializationInclusion(JsonInclude.Include.NON_NULL);
-    private static final String APP_INSIGHTS_ENDPOINT = "https://centralus-2.in.applicationinsights.azure.com/";
-    private static final String APP_INSIGHTS_INSTRUMENTATION_KEY = "1d377c0e-44f8-4d56-bee7-7f13a3fef594";
-    private static final String AZURE_SDK_REPORT_SOURCE = "azure-sdk-intellij-plugin";
+    private static final String AZURE_GROUP_ID = "com.azure";
+
+    private static final String APP_INSIGHTS_CONNECTION_STRING = "InstrumentationKey=1d377c0e-44f8-4d56-bee7-7f13a3fef594;" +
+            "IngestionEndpoint=https://centralus-2.in.applicationinsights.azure.com/;" +
+            "LiveEndpoint=https://centralus.livediagnostics.monitor.azure.com/";
+
     private static final TypeReference<Map<String, Object>> MAP_TYPE_REFERENCE = new TypeReference<>() {
     };
+    public static final String AZURE_SDK_REPORT_ENABLED = "Azure SDK Report Enabled";
+
     private final AtomicReference<BuildReport> currentBuildReport;
-    private final ApplicationInsightsClient applicationInsightsClient;
+    private final TelemetryClient telemetryClient;
     private final ScheduledExecutorService scheduledExecutor = Executors.newSingleThreadScheduledExecutor();
 
     public MavenProjectReportGenerator() {
-        this.applicationInsightsClient = new ApplicationInsightsClientBuilder()
-                .host(APP_INSIGHTS_ENDPOINT)
-                .buildClient();
+        final TelemetryConfiguration configuration = TelemetryConfiguration.createDefault();
+        configuration.setConnectionString(APP_INSIGHTS_CONNECTION_STRING);
+        telemetryClient = new TelemetryClient(configuration);
         this.currentBuildReport = new AtomicReference<>();
-    }
-
-    @Override
-    public void projectClosed(@Nonnull Project project) {
-        System.out.println("Project closed");
-        scheduledExecutor.shutdown();
     }
 
     @Nullable
     @Override
     public Object execute(@Nonnull Project project, @Nonnull Continuation<? super Unit> continuation) {
-        System.out.println("Running the startup activity");
         final MavenProjectsManager mavenProjectsManager = MavenProjectsManager.getInstanceIfCreated(project);
-
         if (mavenProjectsManager == null || !mavenProjectsManager.isMavenizedProject()) {
-            System.out.println("Not a maven project. Exiting.");
             return null;
         }
-        scheduledExecutor.scheduleWithFixedDelay(() -> generateReport(project), 1, 5, TimeUnit.MINUTES);
+        scheduledExecutor.scheduleWithFixedDelay(() -> generateReport(project), 1, 1, TimeUnit.MINUTES);
         return null;
     }
 
+    @Override
+    public void projectClosed(@Nonnull Project project) {
+        scheduledExecutor.shutdown();
+    }
+
     private void generateReport(@Nonnull Project project) {
-        if (Registry.is("Azure SDK Report Enabled", true)) {
-            System.out.println("Running Azure SDK report generation");
-            final BuildReport buildReport = new BuildReport();
-            inspectPomFile(project, buildReport);
-            inspectJavaFiles(project, buildReport);
-            System.out.println("Finished generating report");
+        if (Registry.is(AZURE_SDK_REPORT_ENABLED, true)) {
             try {
-                System.out.println(OBJECT_MAPPER.writeValueAsString(buildReport));
-                if(!buildReport.equals(currentBuildReport.get())) {
+                final BuildReport buildReport = new BuildReport();
+                inspectPomFile(project, buildReport);
+                inspectJavaFiles(project, buildReport);
+                if (!buildReport.equals(currentBuildReport.get())) {
+                    System.out.println(OBJECT_MAPPER.writeValueAsString(buildReport));
                     sendReportToAppInsights(buildReport);
                     currentBuildReport.set(buildReport);
                 } else {
@@ -98,29 +99,19 @@ public final class MavenProjectReportGenerator implements ProjectActivity, DumbA
 
     private void sendReportToAppInsights(BuildReport report) {
         try {
-            TelemetryItem telemetryItem = new TelemetryItem();
-            telemetryItem.setTime(OffsetDateTime.now());
-            telemetryItem.setName(AZURE_SDK_REPORT_SOURCE);
-            telemetryItem.setInstrumentationKey(APP_INSIGHTS_INSTRUMENTATION_KEY);
-            TelemetryEventData data = new TelemetryEventData();
-            Map<String, String> customEventProperties = getCustomEventProperties(report);
-            data.setProperties(customEventProperties);
-            MonitorBase monitorBase = new MonitorBase();
-            monitorBase.setBaseData(data).setBaseType("EventData");
-            data.setName("azure-sdk-java-build-telemetry");
-            telemetryItem.setData(monitorBase);
-            List<TelemetryItem> telemetryItems = new ArrayList<>();
-            telemetryItems.add(telemetryItem);
-            applicationInsightsClient.trackAsync(telemetryItems).block();
+            final EventTelemetry telemetry = new EventTelemetry("azure-sdk-java-intellij-telemetry");
+            telemetry.getProperties().putAll(getCustomEventProperties(report));
+            telemetryClient.track(telemetry);
+            telemetryClient.flush();
             System.out.println("Successfully sent the report to Application Insights");
-        } catch (Exception ex) {
+        } catch (final Exception ex) {
             System.out.println("Unable to send report to Application Insights. " + ex.getMessage());
         }
     }
 
     private Map<String, String> getCustomEventProperties(BuildReport report) {
-        Map<String, Object> properties = OBJECT_MAPPER.convertValue(report, MAP_TYPE_REFERENCE);
-        Map<String, String> customEventProperties = new HashMap<>(properties.size());
+        final Map<String, Object> properties = OBJECT_MAPPER.convertValue(report, MAP_TYPE_REFERENCE);
+        final Map<String, String> customEventProperties = new HashMap<>(properties.size());
         // AppInsights customEvents table does not support nested JSON objects in "properties" field
         // So, we have to convert the nested objects to strings
         properties.forEach((key, value) -> {
@@ -141,12 +132,10 @@ public final class MavenProjectReportGenerator implements ProjectActivity, DumbA
             // Access the content of the file
             final PsiFile psiFile = PsiManager.getInstance(project).findFile(javaFile);
             if (psiFile != null) {
-                System.out.println("Scanning file: " + psiFile.getName());
-                Collection<PsiMethodCallExpression> methodCalls = PsiTreeUtil.findChildrenOfType(psiFile, PsiMethodCallExpression.class);
+                final Collection<PsiMethodCallExpression> methodCalls = PsiTreeUtil.findChildrenOfType(psiFile, PsiMethodCallExpression.class);
                 methodCalls
                         .forEach(methodCall -> {
                             final PsiMethod psiMethod = methodCall.resolveMethod();
-                            System.out.println("Processing method call " + psiMethod.getName());
                             final PsiAnnotation[] annotations = psiMethod.getAnnotations();
                             Arrays.stream(annotations).forEach(annotation -> {
                                 if (Objects.equals(annotation.getQualifiedName(), "com.azure.core.annotation.ServiceMethod")) {
@@ -157,7 +146,6 @@ public final class MavenProjectReportGenerator implements ProjectActivity, DumbA
                                             .collect(Collectors.joining(","));
                                     final String methodSignature = returnType + " " + methodName + "(" + params + ")";
                                     methodCallFrequency.compute(methodSignature, (k, v) -> v == null ? 1 : v + 1);
-                                    System.out.println("Calling a method annotated as service method " + methodSignature);
                                 }
                             });
                         });
@@ -175,26 +163,27 @@ public final class MavenProjectReportGenerator implements ProjectActivity, DumbA
     private void inspectPomFile(@Nonnull Project project, BuildReport buildReport) {
         FileTypeIndex.getFiles(XmlFileType.INSTANCE, GlobalSearchScope.projectScope(project))
                 .forEach(virtualFile -> {
-                    PsiFile file = PsiManager.getInstance(project).findFile(virtualFile);
-                    if (!"pom.xml".equals(file.getName())) {
+                    final PsiFile file = PsiManager.getInstance(project).findFile(virtualFile);
+                    if (file != null && !"pom.xml".equals(file.getName())) {
                         return;
                     }
-                    MavenProjectsManager mavenProjectsManager = MavenProjectsManager.getInstance(file.getProject());
+                    final MavenProjectsManager mavenProjectsManager = MavenProjectsManager.getInstance(file.getProject());
                     if (!mavenProjectsManager.isMavenizedProject()) {
                         return;
                     }
-                    FileViewProvider viewProvider = file.getViewProvider();
-                    XmlFile xmlFile = (XmlFile) viewProvider.getPsi(StdLanguages.XML);
 
-                    XmlTag rootTag = xmlFile.getRootTag();
+                    final FileViewProvider viewProvider = file.getViewProvider();
+                    final XmlFile xmlFile = (XmlFile) viewProvider.getPsi(StdLanguages.XML);
+
+                    final XmlTag rootTag = xmlFile.getRootTag();
                     if (rootTag != null && "project".equals(rootTag.getName())) {
                         final String artifactId = rootTag.findFirstSubTag("artifactId").getValue().getText();
                         final String groupId = rootTag.findFirstSubTag("groupId").getValue().getText();
                         final String version = rootTag.findFirstSubTag("version").getValue().getText();
 
-                        buildReport.setArtifactId(artifactId);
-                        buildReport.setGroupId(groupId);
-                        buildReport.setVersion(version);
+                        buildReport.setArtifactId(getMd5(artifactId));
+                        buildReport.setGroupId(getMd5(groupId));
+                        buildReport.setVersion(getMd5(version));
 
                         checkDependencyManagement(rootTag, buildReport);
                         checkDependencies(rootTag, buildReport);
@@ -217,15 +206,14 @@ public final class MavenProjectReportGenerator implements ProjectActivity, DumbA
                         versionId = dependencyTag.findFirstSubTag("version").getValue().getText();
                     }
 
-                    if ("com.azure".equals(groupId) && artifactId.equals("azure-sdk-bom")) {
+                    if (AZURE_GROUP_ID.equals(groupId) && "azure-sdk-bom".equals(artifactId)) {
                         final String latestArtifactVersion = MavenUtils.getLatestArtifactVersion(groupId, artifactId);
 
                         if (versionId != null && !versionId.equals(latestArtifactVersion)) {
                             buildReport.setBomVersion(versionId);
-                            buildReport.addError(new BuildError("Newer version  of azure-sdk-bom available",
+                            buildReport.addError(new BuildError("Newer version of azure-sdk-bom available",
                                     BuildErrorCode.OUTDATED_DEPENDENCY, BuildErrorLevel.WARNING,
                                     List.of(groupId + ":" + artifactId + ":" + versionId)));
-                            System.out.println("Newer version of azure-sdk-bom is available. Update to version " + latestArtifactVersion);
                         }
                     }
                 }
@@ -249,21 +237,24 @@ public final class MavenProjectReportGenerator implements ProjectActivity, DumbA
                     versionId = dependencyTag.findFirstSubTag("version").getValue().getText();
                 }
 
-                if ("com.azure".equals(groupId)) {
+                if (AZURE_GROUP_ID.equals(groupId)) {
                     azureDependencies.add(groupId + ":" + artifactId + ":" + versionId);
                 }
 
-                if ("com.azure".equals(groupId) && versionId != null && !versionId.contains("beta")) {
+                if (AZURE_GROUP_ID.equals(groupId) && versionId != null && !versionId.contains("beta")) {
                     buildReport.addError(new BuildError("Use Azure SDK Bom (azure-sdk-bom) instead of specifying version in dependency tag",
                             BuildErrorCode.BOM_VERSION_OVERRIDDEN, BuildErrorLevel.WARNING));
                 }
 
-                if ("com.azure".equals(groupId) && versionId != null && versionId.contains("beta")) {
+                if (AZURE_GROUP_ID.equals(groupId) && versionId != null && versionId.contains("beta")) {
                     buildReport.addError(new BuildError("Using a beta version in production is not recommended. Please use a stable version.",
                             BuildErrorCode.BETA_DEPENDENCY_USED, BuildErrorLevel.WARNING));
                 }
             }
         }
+    }
 
+    private String getMd5(String inputText) {
+        return DigestUtils.md5Hex(inputText);
     }
 }
